a563d3bf94090fac82975774431a5762f39ce541
Vasiuta <vstoleksiy@gmail.com>
2021-10-07 23:47:01 +0300

    fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/api/v1/hello-world-4')
 def hello():
-  return "Hello World!"
+  return "Hello World! 4", 200
 
 if __name__ == "__main__":
   app_server = gevent.pywsgi.WSGIServer(('0.0.0.0', 3000), app)
